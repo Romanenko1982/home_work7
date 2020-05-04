93c4d90bcca7183237a2685d538ff273b9e548cd
Romanenko Alexander <Romanenkoxyg@mail.ru>
2020-05-05 01:15:34 +0300

    add ListClass

---
+++ src/arrayList/ListClass.java
@@ -0,0 +1,62 @@
+package arrayList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClass {
+
+  List<String> list = new ArrayList<>();
+
+  public void task(int n) throws IOException {
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    for (int i = 0; i < n; i++) {
+      if (i % 2 == 0) {
+        list.add(0, (reader.readLine()));
+      } else {
+        list.add(reader.readLine());
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (i % 2 == 0) {
+        System.out.println(list.remove(list.size() - 1));
+      } else {
+        System.out.println(list.remove(0));
+      }
+    }
+  }
+
+
+
+  public void creatList(int n) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    for (int i = 0; i < n; i++) {
+      try {
+        list.add(reader.readLine());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void foo() {
+
+    int random = (int) (Math.random() * (list.size() - 2)) + 1;
+    list.add(list.remove(random));
+  }
+
+  @Override
+  public String toString() {
+    return list.toString();
+  }
+
+  public void clear() {
+    list.clear();
+  }
+}
+
+
---
+++ src/arrayList/Main.java
@@ -0,0 +1,11 @@
+package arrayList;
+
+public class Main {
+
+  public static void main(String[] args) {
+    ListClass list = new ListClass();
+    list.creatList(5);
+    list.foo();
+    System.out.println(list.toString());
+  }
+}
